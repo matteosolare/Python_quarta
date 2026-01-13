2733df6f9b2487d4598caa3a99078c58ddc5d391
matteosolare <matteo.solare@itiscuneo.edu.it>
2026-01-13 23:42:00 +0100

    Update DizionariDiListe_voti_per_materia.py

--- Compiti_vacanze/DizionariDiListe_voti_per_materia.py
+++ Compiti_vacanze/DizionariDiListe_voti_per_materia.py
@@ -4,7 +4,33 @@
 
 def calcola_media(voti_materie, materia):
 
-    return sum(voti_materie[materia])/
+    return sum(voti_materie[materia])/len(voti_materie[materia])
+
+
+
+def calcola_media_piu_alta(voti_materie):
+
+    media_max = 0
+
+    for materia in voti_materie:
+        media = sum(voti_materie[materia])/len(voti_materie[materia])
+        for _ in voti_materie[materia]:
+            if media > media_max :
+                media_max = media
+
+    return media_max
+
+
+
+def add_voto(voti_materie, materia):
+
+    voto = 8
+
+    voti_materie[materia].append(voto)
+
+    return voti_materie[materia]
+
+
 
 def main():
 
@@ -14,6 +40,10 @@ def main():
     "Inglese": [8, 8, 9, 7, 8],
     "Informatica": [9, 8, 9, 10, 8]
     }
+
+    print(calcola_media(voti_materie, "Matematica"))
+    print(calcola_media_piu_alta(voti_materie))
+    print(add_voto(voti_materie, "Italiano"))
 
 if __name__ == "__main__":
     main()
